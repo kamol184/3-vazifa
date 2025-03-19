1d635865dda72d415625957e38e7786bc9c6a45d
kamol184 <boburyunusov2017@gmail.com>
2025-03-19 13:10:59 +0500

    Final build

---
+++ src/App.js
@@ -0,0 +1,161 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <header>
+          <h2>WIX</h2>
+          <p>Нажмите «Редактировать», чтобы создать свой сайт</p>
+          <a href="/">
+          Подробнее</a>
+          <button>Редактировать</button>
+          </header>
+
+<section>
+    <div className='container1'>
+      <h2>K. Griffith</h2>
+          <li>apearances</li>
+          <li>books</li>
+          <li>news</li>
+          <li>about</li>
+          <li>contact</li>
+    </div>
+
+    <div className='kaylaauthor'>
+      <h1 className='kaylatext'>Kayla Griffith</h1>
+      <p>Award Winning Author</p>
+      <img src='https://static.wixstatic.com/media/ad420a_ca05081d4ece492fb6589c81baee1091~mv2.jpg/v1/crop/x_1416,y_0,w_3091,h_3414/fill/w_298,h_329,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/GettyImages-11967148821.jpg' alt='rasm bor'></img>
+      <img src='https://static.wixstatic.com/media/c4392d634a0148fda8b7b2b0ad98293b.png/v1/fill/w_31,h_31,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/c4392d634a0148fda8b7b2b0ad98293b.png' className='icon1' alt='rasm bor'></img>
+      <img src='https://static.wixstatic.com/media/e0678ef25486466ba65ef6ad47b559e1.png/v1/fill/w_31,h_31,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/e0678ef25486466ba65ef6ad47b559e1.png' className='icon2' alt='rasm bor'></img>
+    </div>
+    </section>
+
+  <section>
+    <div className='containertheswanisle'>
+    <div className='theswanisle'>
+      <div className='theswanisleleft'>
+        <div className='newrelease'><p>New Release</p></div>
+        <h3>The Swan Isle <br/>
+          (2035)</h3>
+        <p>I'm a paragraph. Click here to add your <br/>
+         own text and edit me. It’s easy. Just click<br/>
+          “Edit Text” or double click me to add<br/>
+          your own content and make changes to<br/>
+          the font. I’m a great place for you to tell <br/> 
+          a story and let your users know a little <br/>
+           more about you.</p>
+           <div>
+            <p className='ordernow'>ORDER NOW</p>
+            <button>Amazon</button>
+            <button>Google</button>
+            <button>ibooks</button>
+           </div>
+      </div>
+      <img src='https://static.wixstatic.com/media/ad420a_ac6bbfd46dbe4c83a234221d29d67791~mv2.png/v1/fill/w_481,h_679,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Screen%20Shot%202021-02-07%20at%209_24_47.png' className='swanilse' alt='rasm bor'></img>  
+    </div>
+    </div>
+    </section>
+
+    <section>
+      <div className='container2'>
+        <div className='praise'>
+        <h1 className='textpraise'>Praise & Reviews</h1>
+         {/* <span className='small'>''</span> */}
+        <div className='rewiews'>
+          <div className='review'>
+          <span className='small'>''</span>
+          <p>I'm a paragraph. Click here to add your <br /> 
+          own text and edit me. It’s easy. Just click <br /> 
+          “Edit Text” or double click me to add <br /> 
+          your own content and make changes to <br /> 
+          the font. I’m a great place for you to tell <br />
+           a story and let your users know a little <br />
+           more about you.</p>
+           <p className='text1'>‘Swan Isle’ is Griffith's <br />
+            best writing yet” The <br /> Times Book Review</p>
+            <hr />
+        </div>
+
+        <div className='review'>
+        <span className='small'>''</span>
+          <p>I'm a paragraph. Click here to add your <br /> 
+          own text and edit me. It’s easy. Just click <br /> 
+          “Edit Text” or double click me to add <br /> 
+          your own content and make changes to <br /> 
+          the font. I’m a great place for you to tell <br />
+           a story and let your users know a little <br />
+           more about you.</p>
+           <p className='text1'>‘Gripping storytelling’ <br />
+           The Good Read Blog</p>
+           <hr />
+        </div>
+
+        <div className='review'>
+        <span className='small'>''</span>
+          <p>I'm a paragraph. Click here to add your <br /> 
+          own text and edit me. It’s easy. Just click <br /> 
+          “Edit Text” or double click me to add <br /> 
+          your own content and make changes to <br /> 
+          the font. I’m a great place for you to tell <br />
+           a story and let your users know a little <br />
+           more about you.</p>
+           <p className='text1'>Grifith’s voice tells the <br />
+            story of all women “The <br /> Seattle Post Review”</p>
+            <hr />
+        </div>
+        </div>
+        </div>
+        </div>
+    </section>
+
+    <section>
+      <div className='seeupcoming'>
+        <div className='seeclick'>
+          <h2>See Upcoming Appearances</h2>
+          <p className='seetext'>I'm a paragraph. Click here to add your own <br />
+            text and edit me. It's easy.</p>
+          <div className='january'>
+            <p className='textjanuary'>
+            January 18th 2035, The Breakfast Club, Virtual Book Reading <br />
+             of Swan Isle, 7PM - 8PM EST</p>
+             <button>Join</button>
+        </div>
+        <div className='january'>
+            <p className='textjanuary'>January 31st 2035, The Good Read Club, Online <br />
+             Conversation with Kayla Griffith, 8PM - 9PM EST
+            </p>
+             <button>Join</button>
+        </div>
+        <div className='january'>
+            <p className='textjanuary'>March 2nd 2035, Otto Cafe, Online Book Reading <br /> 
+             With Kayla Griffith, 3PM - 4PM EST
+            </p>
+             <button>Join</button>
+        </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div className='aboutkayla'>
+        <img src="https://static.wixstatic.com/media/ad420a_3b0ac18b8f8c44e6968cf137c044b3b4~mv2.jpg/v1/crop/x_1678,y_0,w_3414,h_3414/fill/w_221,h_221,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/GettyImages-11967148791.jpg" alt="kayla rasmi bor" />
+        <h2>About Kayla Griffith</h2>
+        <div>
+          <p>I'm a paragraph. Click here to add your own text and <br />
+            edit me. It’s easy. Just click “Edit Text” or double click <br />
+             me to add your own content and make changes to the <br /> font. I’m a great place for you to tell a story and let <br />
+              your users know a little more about you.</p>
+              <button>Read More</button>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <p>© 2035 by K.Griffith. Powered and secured by <a href="/">Wix</a> </p>
+    </footer>
+    </div>
+  );
+}
+
+export default App;
